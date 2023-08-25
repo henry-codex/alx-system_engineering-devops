87e9c5461b984aa0e13a668b857ec3854ccd59fe
henry-codex <httetteh@st.ug.edu.gh>
2023-08-25 13:34:46 -0700

    configuration

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# kills a process named killmenow
+exec { 'killmenow':
+  command => 'pkill -n killmenow',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test `pgrep killmenow|wc -l` -ge 1',
+}
